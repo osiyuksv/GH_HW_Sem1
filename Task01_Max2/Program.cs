585185bf1146eddaecdfb09fb9d5b7ff55187a0b
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 08:52:04 +0300

    Task01_Max2

---
+++ Task01_Max2/Program.cs
@@ -0,0 +1,22 @@
+﻿// программа, 2 числа на вход, на выходе макс и мин
+Console.WriteLine("Введите первое чиcло");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чиcло");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if(number1 > number2)
+{
+max = number1;
+min = number2;
+}
+else
+{
+max = number2;
+min = number1;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);

39de009b1b28472907d29cc83e6742e526e83282
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 09:16:37 +0300

    Task 02 Max3

---
+++ Task02_Max3/Program.cs
@@ -0,0 +1,16 @@
+﻿// выводим макс из трёх чисел
+
+Console.WriteLine("Введите первое чиcло");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чиcло");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье чиcло");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(number1 > max) max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.Write("max = ");
+Console.WriteLine(max);

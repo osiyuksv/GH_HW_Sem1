fdded7c29eed1208e7edad3d5f17a8cf4a8da7e5
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 09:50:39 +0300

    Task 04 Even List

---
+++ Task04_EvenList/Program.cs
@@ -0,0 +1,14 @@
+﻿// на входе число N, на выходе список всех чётных от 1 до N
+Console.WriteLine("Введите чиcло");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(count < number)
+{
+if(count % 2 == 0) 
+{
+  Console.Write(count);
+  Console.Write("; ");
+}
+count ++;
+}

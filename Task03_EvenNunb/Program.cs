363dd9f4df874da21608b553d2cbe815f2d916e0
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 09:31:29 +0300

    Task 03 Even Number

---
+++ Task03_EvenNunb/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, определяет чётность числа
+Console.WriteLine("Введите чиcло");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+Console.Write(number);
+Console.WriteLine(" - это чётное число");
+}
+else
+{
+Console.Write(number);
+Console.WriteLine(" - это НЕчётное число");
+}
